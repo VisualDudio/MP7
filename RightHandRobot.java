16e9cbbc53e20aedd5ad9ba8f1e4e23162d23422
Omar Mbarki <omar.mbarki5@gmail.com>
2017-12-11 21:15:57 -0600

    reformat

--- RightHandRobot.java
+++ RightHandRobot.java
@@ -1,88 +1,85 @@
-import java.util.*;
 public class RightHandRobot extends Robot
 {
+    private int orientation;
+    
     public RightHandRobot(int startX, int startY, int orientation)
     {
         super(startX, startY);
-        this.orientation=orientation; 
+        this.orientation = orientation; 
     }
     
-    public void move(Maze m)
+    public void move(Maze maze)
     {
        int currentYTemp = getRobotPos()[1]; 
        int currentXTemp = getRobotPos()[0];
-        switch (possibleMove(m)) {
-           case "Up":
-                 currentYTemp--;
-                 break;
-           case "Down":
-                 currentYTemp++;
-                 break;
-           case "Right":
-                 currentXTemp++;
-                 break;
-           case "Left":
-                 currentXTemp--;
-                 break;
+       switch (possibleMove(maze)) {
+	       case "Up":
+	    	   currentYTemp--;
+	    	   break;
+	       case "Down":
+	           currentYTemp++;
+	           break;
+	       case "Right":
+	           currentXTemp++;
+	           break;
+	       case "Left":
+	           currentXTemp--;
+	           break;
        }
+       
        setPos(currentXTemp, currentYTemp);
     }
     
-    public String possibleMove(Maze m)
+    public String possibleMove(Maze maze)
     {
-        boolean[][] temp = m.getMaze();
-        String move="";
+        boolean[][] temp = maze.getMaze();
         int currentYTemp = getRobotPos()[1]; 
         int currentXTemp = getRobotPos()[0];
-        for (int i = 0; i<2; i++)
-        {
-            if(orientation == 3)//South
+        
+        for (int i = 0; i < 2; i++) {
+            if (orientation == 3)//South
             {
-                if(currentYTemp+1!=temp.length && temp[currentYTemp+1][currentXTemp] && !temp[currentYTemp][currentXTemp-1])
+                if(currentYTemp + 1 != temp.length && temp[currentYTemp + 1][currentXTemp] && !temp[currentYTemp][currentXTemp - 1])
                     return "Down";
-                    else if(currentXTemp+1!=temp[0].length && !temp[currentYTemp][currentXTemp-1])
+                else if(currentXTemp + 1 != temp[0].length && !temp[currentYTemp][currentXTemp-1])
                     orientation++;
-                    else
-                    {
-                        orientation--;
-                        return "Left";
-                    }
-            }
-        
-            if(orientation==4)//East
-            {
-                if(currentXTemp+1!=temp[0].length && temp[currentYTemp][currentXTemp+1] && !temp[currentYTemp+1][currentXTemp])
-                    return "Right";
-                    else if(currentYTemp!=0 && !temp[currentYTemp+1][currentXTemp])
-                    orientation=1;
-                    else
-                    {
-                        orientation--;
-                        return "Down";
-                    }
+                else {
+                    orientation--;
+                    return "Left";
                 }
-        
-            if(orientation==1)//North
-            {
-                if(currentYTemp!=0 && temp[currentYTemp-1][currentXTemp] && !temp[currentYTemp][currentXTemp+1])
-                   return "Up";
-                   else if(currentXTemp!=0 && !temp[currentYTemp][currentXTemp+1])
-                   orientation++;
-                   else
-                   {
-                        orientation=4; 
-                        return "Right";
-                   }
             }
         
-            if(orientation==2)//West
+            if (orientation == 4)//East
             {
-                if(currentXTemp!=0 && temp[currentYTemp][currentXTemp-1] && !temp[currentYTemp-1][currentXTemp])
+                if(currentXTemp + 1 != temp[0].length && temp[currentYTemp][currentXTemp + 1] && !temp[currentYTemp + 1][currentXTemp])
+                    return "Right";
+                else if(currentYTemp != 0 && !temp[currentYTemp + 1][currentXTemp])
+                    orientation = 1;
+                else {
+                    orientation--;
+                    return "Down";
+                }
+            }
+        
+            if (orientation == 1)//North
+            {
+                if (currentYTemp != 0 && temp[currentYTemp - 1][currentXTemp] && !temp[currentYTemp][currentXTemp + 1])
+                	return "Up";
+                else if(currentXTemp != 0 && !temp[currentYTemp][currentXTemp + 1])
+                    orientation++;
+                else {
+                    orientation = 4; 
+                    return "Right";
+                }
+            }
+        
+            if(orientation == 2)//West
+            {
+                if (currentXTemp != 0 && temp[currentYTemp][currentXTemp - 1] && !temp[currentYTemp - 1][currentXTemp])
                    return "Left";
-                else if(currentYTemp+1!=temp.length && !temp[currentYTemp-1][currentXTemp])
+                else if (currentYTemp + 1 != temp.length && !temp[currentYTemp - 1][currentXTemp])
                    orientation++;
-                else  
-                {
+                else {
                    orientation--;
                    return "Up";
                 }
@@ -94,9 +91,9 @@ public class RightHandRobot extends Robot
     //West=2
     //South=3
     //East=4
+    
     public int getOrientation()
     {
         return orientation;
     }
-    private int orientation; 
 }
