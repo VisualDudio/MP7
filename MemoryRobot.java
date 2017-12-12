f137402241f52a95848f1c27ad01996ace1e5905
Omar Mbarki <omar.mbarki5@gmail.com>
2017-12-11 21:14:42 -0600

    reformat

--- MemoryRobot.java
+++ MemoryRobot.java
@@ -1,19 +1,17 @@
 import java.util.*;
-public class MemoryRobot extends RandomRobot
-{
-   public MemoryRobot(int startX, int startY)
-   {
+
+public class MemoryRobot extends RandomRobot {
+   public MemoryRobot(int startX, int startY) {
        super(startX, startY);
    }
    
-   public void move(Maze m)
-   {
+   public void move(Maze maze) {
        int previousX = getRobotPos()[0];
        int previousY = getRobotPos()[1];
-       ArrayList<String> moves = possibleMoves(m);
-       super.move(m);
+       ArrayList<String> moves = possibleMoves(maze);
+       super.move(maze);
        if (moves.size() == 1) {
-           m.getMaze()[previousY][previousX]=false;
+           maze.getMaze()[previousY][previousX] = false;
        }
    }
 }    
