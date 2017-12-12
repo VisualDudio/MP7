a57850d960f3b417b8334fb34514ff1649f9c3db
Omar Mbarki <omar.mbarki5@gmail.com>
2017-12-11 21:15:23 -0600

    reformat

--- RandomRobot.java
+++ RandomRobot.java
@@ -1,53 +1,50 @@
 import java.util.*;
-public class RandomRobot extends Robot
-{
-   public RandomRobot(int startX, int startY)
-   {
+public class RandomRobot extends Robot {
+   public RandomRobot(int startX, int startY) {
        super(startX, startY);
    }
    
-   public void move(Maze m)
-   {
+   public void move(Maze m) {
        int currentYTemp = getRobotPos()[1]; 
        int currentXTemp = getRobotPos()[0];
        ArrayList<String> moves = possibleMoves(m);
        Random gen = new Random();
-       switch (moves.get(gen.nextInt(moves.size())))
-       {
+       switch (moves.get(gen.nextInt(moves.size()))) {
            case "Up":
-                 currentYTemp--;
-                 break;
+        	   currentYTemp--;
+        	   break;
            case "Down":
-                 currentYTemp++;
-                 break;
+               currentYTemp++;
+               break;
            case "Right":
-                 currentXTemp++;
-                 break;
+               currentXTemp++;
+               break;
            case "Left":
-                 currentXTemp--;
-                 break;
+               currentXTemp--;
+               break;
        }
+       
        setPos(currentXTemp, currentYTemp);
    }
     
-   public ArrayList<String> possibleMoves(Maze m)
-   {
+   public ArrayList<String> possibleMoves(Maze m) {
        boolean [][] temp = m.getMaze(); 
-       ArrayList<String> moves=new ArrayList<String>();
+       ArrayList<String> moves = new ArrayList<String>();
        int currentYTemp = getRobotPos()[1]; 
        int currentXTemp = getRobotPos()[0];
-       if (currentYTemp != temp.length-1 && temp[currentYTemp+1][currentXTemp]) {
-                moves.add("Down");
-       }
-       if (currentYTemp != 0 && temp[currentYTemp-1][currentXTemp]) {
-                moves.add("Up");
-       }
-       if(currentXTemp != temp[0].length && temp[currentYTemp][currentXTemp+1]) {
-                moves.add("Right");
-       }
-       if(currentXTemp != 0 && temp[currentYTemp][currentXTemp-1]) {
-                moves.add("Left");
-       }
+       
+       if (currentYTemp != temp.length - 1 && temp[currentYTemp + 1][currentXTemp])
+    	   moves.add("Down");
+       
+       if (currentYTemp != 0 && temp[currentYTemp - 1][currentXTemp])
+           moves.add("Up");
+     
+       if(currentXTemp != temp[0].length && temp[currentYTemp][currentXTemp + 1])
+           moves.add("Right");
+       
+       if(currentXTemp != 0 && temp[currentYTemp][currentXTemp - 1])
+           moves.add("Left");
+       
        return moves;
    }
 }    
